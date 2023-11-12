9a0a9ecc04d654fc4f8ad2839db9bd95eff2ac84
조해창 <outofchng@gmail.com>
2023-11-13 06:13:35 +0900

    top nav 오류 보류

--- modules/home/TopNavigation.tsx
+++ modules/home/TopNavigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { IconButton, Section } from "@radix-ui/themes";
-import { usePathname, useRouter } from "next/navigation";
+import { Flex, IconButton, Section } from "@radix-ui/themes";
+import { usePathname, useRouter, useSelectedLayoutSegments } from "next/navigation";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
@@ -9,15 +9,19 @@ export function TopNavigation() {
   const pathname = usePathname();
   const router = useRouter();
 
+  let fontSize: "large" | "medium" = "large";
+  let px: "4" | "8" = "8";
+  const selectedLayoutSegments = useSelectedLayoutSegments();
+  if (selectedLayoutSegments.some((segment) => segment === "m")) {
+    fontSize = "medium"; // 모바일이면 크기를 medium으로 변경
+    px = "4";
+  }
+
   return (
     <>
-      <Section
-        pb="6"
-        pt="6"
-        px="4"
-        style={{ display: "flex", justifyContent: pathname === "/" ? "flex-end" : "flex-start" }}
-      >
-        {pathname !== "/" ? (
+      <Section pb="6" pt="6" px={px}>
+        {/* <Flex justify={pathname === "/" ? "end" : "start"}> */}
+        {pathname !== "/" && (
           <IconButton
             variant="ghost"
             color="gray"
@@ -26,20 +30,11 @@ export function TopNavigation() {
               router.back();
             }}
           >
-            <ArrowBackIcon fontSize="medium" />
-          </IconButton>
-        ) : (
-          <IconButton
-            variant="ghost"
-            color="gray"
-            size="4"
-            onClick={() => {
-              window.scrollTo(0, document.body.scrollHeight);
-            }}
-          >
-            <ShoppingCartIcon fontSize="medium" />
+            <ArrowBackIcon fontSize={fontSize} />
           </IconButton>
         )}
+
+        {/* </Flex> */}
       </Section>
     </>
   );
