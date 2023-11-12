17b27d87c2de8feca022d2b169c4103db591ebb5
조해창 <outofchng@gmail.com>
2023-11-12 23:18:13 +0900

    next config prefetch 제외 추가

--- next.config.js
+++ next.config.js
@@ -1,35 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/m/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-      {
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
   async rewrites() {
     return [
       {
@@ -39,6 +9,14 @@ const nextConfig = {
       {
         source: "/:path*",
         destination: "http://www.aladin.co.kr/ttb/api/:path*",
+      },
+      {
+        source: "/m/:path*__index",
+        destination: "/:path*",
+      },
+      {
+        source: "/:path*__index",
+        destination: "/:path*",
       },
     ];
   },
