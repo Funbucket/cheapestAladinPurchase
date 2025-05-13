4ab11b212ec34350656c0a53c60c2a026bff935f
조해창 <outofchng@gmail.com>
2025-05-13 17:20:12 +0900

    Update wc script

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ import { ToastProvider } from "@/modules/home/ToastProvider";
 import { TopNavigation } from "@/modules/home/TopNavigation";
 import GTM from "@/modules/GTM";
 import { Suspense } from "react";
-import WC from "@/modules/WC";
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -28,22 +27,23 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning className="dark-theme">
       <head>
-        <script
+        <Script
+          id="actionspeak"
+          src="https://actionspeak.kr/js/script.js"
+          data-domain="checkmoa.site"
+          strategy="beforeInteractive"
+        />
+        <Script
+          id="wc-script"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `
-      var script = document.createElement('script');
-      script.defer = true;
-      script.dataset.domain = "checkmoa.site";
-      script.src = "https://actionspeak.kr/js/script.js";
-      document.head.appendChild(script);
-    `,
+            __html: `(function(e,n,t,r){var u;var c=o()+"ver="+f(n);a();function i(){var n=document.getElementsByTagName(e)[0];var t=document.createElement(e);t["async"]=true;t.charset="utf-8";t.src=c;n.parentNode.insertBefore(t,n)}function o(){return window.location.protocol=="https:"?"https://work.nethru.co.kr/static/222242/install.js?":"http://work.nethru.co.kr/static/222242/install.js?"}function f(n){var t=(new Date).getTime();var e=n*1e3*60;return n>0?Math.floor(t/e)*e:t}function a(){var n=l();if(!n)return false;n.open("GET",c,true);n.withCredentials=r;n.send(null);n.onload=function(){s();i()};n.onerror=n.onabort=function(){s()};v(function(){n.abort()},t)}function l(){var n;var t=[function(){return new XMLHttpRequest()}];for(var e=0;e<t.length;e++){try{n=t[e]()}catch(r){continue}break}return n}function v(n,t){u=setTimeout(n,t)}function s(){if(u){clearTimeout(u);u=null}}})("script",1440,1000,false);`,
           }}
         />
       </head>
       <body className={`${inter.className} dark`}>
         <Suspense>
           <GTM />
-          <WC />
         </Suspense>
         <Theme appearance="dark">
           <ToastProvider>
